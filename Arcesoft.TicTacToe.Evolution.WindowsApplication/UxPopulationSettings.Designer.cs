672d2e3d6068f4ecde5f38315f2899856ca20e15
Jose Arce <jose.arce@mindbodyonline.com>
2018-02-21 17:38:58 -0800

    stuff

---
+++ Arcesoft.TicTacToe.Evolution.WindowsApplication/UxPopulationSettings.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Arcesoft.TicTacToe.Evolution.WindowsApplication
+{
+    partial class UxPopulationSettings
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSourceMain = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSourceMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSourceMain
+            // 
+            this.bindingSourceMain.DataSource = typeof(Arcesoft.TicTacToe.Evolution.PopulationSettings);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // UxPopulationSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "UxPopulationSettings";
+            this.Size = new System.Drawing.Size(431, 334);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSourceMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSourceMain;
+        private System.Windows.Forms.Label label1;
+    }
+}
